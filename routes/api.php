53760f07201f8ae05d4d52a04202f91caf999d5f
subashc069 <bcoolboy8@gmail.com>
2020-08-21 18:23:14 +0545

    passport auth

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
-Route::get('/test', function(Request $request){
-    return "Hello from api";
+Route::prefix('auth')->group(function (){
+    Route::post('/register', 'AuthController@register');
+    Route::post('/login', 'AuthController@login');
+    Route::get('/logout', 'AuthController@logout')->middleware('auth:api');
+    Route::get('/user', 'AuthController@user')->middleware('auth:api');
 });
